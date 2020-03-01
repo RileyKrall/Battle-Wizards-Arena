191ec09f74ddadc1e32ad8ab53bd1e5479539e15
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2020-02-29 16:17:34 -0800

    Update VoxelManager.cpp

--- VoxelManager.cpp
+++ VoxelManager.cpp
@@ -1,4 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Voxel Manager is the overall manager of voxel coordinate planes. Each coordinate plane is represented by the VGrid.
+// When needing access to a specific grid, you will contact your maps voxel manager.
 
 
 #include "VoxelManager.h"
@@ -24,20 +25,20 @@ void AVoxelManager::BeginPlay()
 	UWorld* const World = GetWorld();
 
 	this->SetOwner(GetWorld()->GetFirstPlayerController());
-	// if the server put manager pointer on game mode
 
 	FActorSpawnParameters ActorSpawnParams;
 	ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
 	FVector loc = FVector(0, 0, 0);
 	FRotator rot = FRotator(0, 0, 0);
-
+	
+	// Place pointer to this, into the local first player controller, allowing for the player to edit voxels
 	AWizardPlayerController* PC = Cast<AWizardPlayerController>(World->GetFirstPlayerController());
 	if (IsValid(PC)) {
 		PC->VoxMan = this;
 	}
 	
-
+	// Temporary implementation. This spawns a single instance on the map, will be the map terrain.
 	Grids.Add(World->SpawnActor<AVGrid>(loc, rot, ActorSpawnParams));
 	Grids[0]->SetID(0);
 
